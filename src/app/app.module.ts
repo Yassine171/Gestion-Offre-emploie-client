095339649dec37d74983afaf35ee2b9894dbe2cc
Yassine <Oussiyassine1@gmail.com>
2023-01-18 18:28:12 +0100

    client

---
+++ src/app/app.module.ts
@@ -0,0 +1,60 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { WebScrapingComponent } from './components/web-scraping/web-scraping.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RecomendationComponent } from './components/recomendation/recomendation.component';
+import { ModelComponent } from './components/model/model.component';
+// import {MatButtonModule} from '@angular/material/button';
+// import {MatTableModule} from '@angular/material/table';
+// import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+// import {MatPaginatorModule} from '@angular/material/paginator';
+
+
+
+const routes :Routes = [{path: 'Login', component: LoginComponent},
+{path: 'web-scraping', component: WebScrapingComponent},
+{path: '', redirectTo: '/Login', pathMatch: 'full'},
+{path: 'home', component: HomeComponent},
+{path: 'train-model', component: ModelComponent}
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    WebScrapingComponent,
+    NavbarComponent,
+    FooterComponent,
+    RecomendationComponent,
+    ModelComponent,
+
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+
+
+
+
+
+    RouterModule.forRoot(
+      routes,)
+  ],
+
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
